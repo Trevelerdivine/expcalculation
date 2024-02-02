57b73e7945c21884755b2e76b9a4d703b072d595
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-02-02 15:45:09 +0900

    test

--- script2.js
+++ script2.js
@@ -1952,35 +1952,6 @@ async function monte_carlo_calculate()
 
   calculationMessage.style.visibility = "hidden";
 
-  let dlthpScore = document.getElementById("dlt_hp_score");
-  let dltAfhp = document.getElementById("dlt_af_hp");
-  let dltdeffScore = document.getElementById("dlt_deff_score");
-  let dltAfdeff = document.getElementById("dlt_af_deff");
-  let dltElmScore = document.getElementById("dlt_elm_score");
-  let dltAfElm = document.getElementById("dlt_af_elm");
-  let dltelmchargeScore = document.getElementById("dlt_elm_charge_score");
-  let dltAfelmcharge = document.getElementById("dlt_af_elm_charge");
-  let dltattckScore = document.getElementById("dlt_attck_score");
-  let dltAfattck = document.getElementById("dlt_af_attck");
-  let dltcrScore = document.getElementById("dlt_cr_score");
-  let dltAfcr = document.getElementById("dlt_af_cr");
-  let dltcdScore = document.getElementById("dlt_cd_score");
-  let dltAfcd = document.getElementById("dlt_af_cd");
-  dlthpScore.style.color = "black";
-  dltAfhp.style.color = "black";
-  dltdeffScore.style.color = "black";
-  dltAfdeff.style.color = "black";
-  dltElmScore.style.color = "black";
-  dltAfElm.style.color = "black";
-  dltelmchargeScore.style.color = "black";
-  dltAfelmcharge.style.color = "black";
-  dltattckScore.style.color = "black";
-  dltAfattck.style.color = "black";
-  dltcrScore.style.color = "black";
-  dltAfcr.style.color = "black";
-  dltcdScore.style.color = "black";
-  dltAfcd.style.color = "black";
-
   if (depend_status[0] == 1)
   {
     //document.getElementById("my_result_hp").innerHTML = my_result_status[0].toFixed(0);
@@ -1998,11 +1969,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_hp").innerHTML = "-";
     //document.getElementById("my_hp_score").innerHTML = "-";
     document.getElementById("appro_hp_score").innerHTML = "-";
-    document.getElementById("dlt_hp_score").innerHTML = "-";
+    //document.getElementById("dlt_hp_score").innerHTML = "-";
     document.getElementById("count_hp_score").innerHTML = "-";
     //document.getElementById("my_af_hp").innerHTML = "-";
     document.getElementById("appro_af_hp").innerHTML = "-";
-    document.getElementById("dlt_af_hp").innerHTML = "-";
+    //document.getElementById("dlt_af_hp").innerHTML = "-";
     document.getElementById("count_hp_score3").innerHTML = "-";
   }
 
@@ -2023,11 +1994,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_deff").innerHTML = "-";
     //document.getElementById("my_deff_score").innerHTML = "-";
     document.getElementById("appro_deff_score").innerHTML = "-";
-    document.getElementById("dlt_deff_score").innerHTML = "-";
+    //document.getElementById("dlt_deff_score").innerHTML = "-";
     document.getElementById("count_deff_score").innerHTML = "-";
     //document.getElementById("my_af_deff").innerHTML = "-";
     document.getElementById("appro_af_deff").innerHTML = "-";
-    document.getElementById("dlt_af_deff").innerHTML = "-";
+    //document.getElementById("dlt_af_deff").innerHTML = "-";
     document.getElementById("count_deff_score3").innerHTML = "-";
   }
 
@@ -2048,11 +2019,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_elm").innerHTML = "-";
     //document.getElementById("my_elm_score").innerHTML = "-";
     document.getElementById("appro_elm_score").innerHTML = "-";
-    document.getElementById("dlt_elm_score").innerHTML = "-";
+    //document.getElementById("dlt_elm_score").innerHTML = "-";
     document.getElementById("count_elm_score").innerHTML = "-";
     //document.getElementById("my_af_elm").innerHTML = "-";
     document.getElementById("appro_af_elm").innerHTML = "-";
-    document.getElementById("dlt_af_elm").innerHTML = "-";
+    //document.getElementById("dlt_af_elm").innerHTML = "-";
     document.getElementById("count_elm_score3").innerHTML = "-";
   }
 
@@ -2073,11 +2044,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_elm_charge").innerHTML = "-";
     //document.getElementById("my_elm_charge_score").innerHTML = "-";
     document.getElementById("appro_elm_charge_score").innerHTML = "-";
-    document.getElementById("dlt_elm_charge_score").innerHTML = "-";
+    //document.getElementById("dlt_elm_charge_score").innerHTML = "-";
     document.getElementById("count_elm_charge_score").innerHTML = "-";
     //document.getElementById("my_af_elm_charge").innerHTML = "-";
     document.getElementById("appro_af_elm_charge").innerHTML = "-";
-    document.getElementById("dlt_af_elm_charge").innerHTML = "-";
+    //document.getElementById("dlt_af_elm_charge").innerHTML = "-";
     document.getElementById("count_elm_charge_score3").innerHTML = "-";
   }
 
@@ -2098,11 +2069,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_attck").innerHTML = "-";
     //document.getElementById("my_attck_score").innerHTML = "-";
     document.getElementById("appro_attck_score").innerHTML = "-";
-    document.getElementById("dlt_attck_score").innerHTML = "-";
+    //document.getElementById("dlt_attck_score").innerHTML = "-";
     document.getElementById("count_attck_score").innerHTML = "-";
     //document.getElementById("my_af_attck").innerHTML = "-";
     document.getElementById("appro_af_attck").innerHTML = "-";
-    document.getElementById("dlt_af_attck").innerHTML = "-";
+    //document.getElementById("dlt_af_attck").innerHTML = "-";
     document.getElementById("count_attck_score3").innerHTML = "-";
   }
 
@@ -2123,11 +2094,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_cr").innerHTML = "-";
     //document.getElementById("my_cr_score").innerHTML = "-";
     document.getElementById("appro_cr_score").innerHTML = "-";
-    document.getElementById("dlt_cr_score").innerHTML = "-";
+    ///document.getElementById("dlt_cr_score").innerHTML = "-";
     document.getElementById("count_cr_score").innerHTML = "-";
     //document.getElementById("my_af_cr").innerHTML = "-";
     document.getElementById("appro_af_cr").innerHTML = "-";
-    document.getElementById("dlt_af_cr").innerHTML = "-";
+    //document.getElementById("dlt_af_cr").innerHTML = "-";
     document.getElementById("count_cr_score3").innerHTML = "-";
   }
 
@@ -2152,11 +2123,11 @@ async function monte_carlo_calculate()
     document.getElementById("appro_result_cd").innerHTML = "-";
     //document.getElementById("my_cd_score").innerHTML = "-";
     document.getElementById("appro_cd_score").innerHTML = "-";
-    document.getElementById("dlt_cd_score").innerHTML = "-";
+    //document.getElementById("dlt_cd_score").innerHTML = "-";
     document.getElementById("count_cd_score").innerHTML = "-";
     //document.getElementById("my_af_cd").innerHTML = "-";
     document.getElementById("appro_af_cd").innerHTML = "-";
-    document.getElementById("dlt_af_cd").innerHTML = "-";
+    //document.getElementById("dlt_af_cd").innerHTML = "-";
     document.getElementById("count_cd_score3").innerHTML = "-";
   }
 

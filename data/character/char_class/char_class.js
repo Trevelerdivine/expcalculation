d3fa2662b0212168ad7c8c34b63a2d79ae00ac05
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-03-24 01:52:45 +0900

    test

--- data/character/char_class/char_class.js
+++ data/character/char_class/char_class.js
@@ -1561,8 +1561,16 @@ class gaming {
     }
     else
     {
-      attckRate = status[4] * dmg_rate[4]+ calculate_weapon_basedmg(this.react_attack_count + this.nonreact_attack_count, status, this.weapon_rank, this.base_dmgbuff);
-      basicDmg = attckRate;
+      if (attack_method == 1 || attack_method == 6)
+      {
+        attckRate = status[4] * dmg_rate[4]+ calculate_weapon_basedmg(this.react_attack_count + this.nonreact_attack_count, status, this.weapon_rank, this.base_dmgbuff);
+        basicDmg = attckRate;
+      }
+      else
+      {
+        attckRate = status[4] * (dmg_rate[4][0] + dmg_rate[4][1]) + calculate_weapon_basedmg(this.react_attack_count + this.nonreact_attack_count, status, this.weapon_rank, this.base_dmgbuff);
+        basicDmg = attckRate;
+      }
     }
     return basicDmg;
   }

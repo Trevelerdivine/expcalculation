66d7f0927470a31c211d1a58a2fce38c0639c2d4
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-04-14 00:23:47 +0900

    test

--- script.js
+++ script.js
@@ -2556,7 +2556,7 @@ async function monte_carlo_calculate()
     document.getElementById(ClockId).innerHTML = main_status_name[ExpDmgList[i-1][1][0]];
     document.getElementById(GobletId).innerHTML = main_status_name[ExpDmgList[i-1][1][1]];
     document.getElementById(CircletId).innerHTML = main_status_name[ExpDmgList[i-1][1][2]];
-    document.getElementById(DmgrateId).innerHTML = (ExpDmgList[0][0] * 100/ExpDmgList[i-1][0]).toFixed(1) + "％";
+    document.getElementById(DmgrateId).innerHTML = (ExpDmgList[i-1][0] * 100/ExpDmgList[0][0]).toFixed(1) + "％";
 
 }
 

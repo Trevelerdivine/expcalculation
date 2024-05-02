39c89d36acbff9a35d50d24a9370532da03a605c
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-05-03 02:10:37 +0900

    test

--- script5.js
+++ script5.js
@@ -96,6 +96,7 @@ const elm_reaction_obj = [
 
 async function calculate_char_base_status() 
 {
+    //UserData.data.playerInfo.showAvatarInfoList.
   const char_level = document.getElementById("char_level").value;
   const response = await fetch("./data/character/char_data/" + char_name[selectedCharId] + ".json");
   const data = await response.json();

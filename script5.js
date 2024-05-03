de479ccd2e2b1cc0478754c8c50280114faabd96
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-05-03 12:27:48 +0900

    test

--- script5.js
+++ script5.js
@@ -98,18 +98,18 @@ async function calculate_char_base_status()
 {
   const response = await fetch("../data/character/char_data/" + char_name[selectedCharId] + ".json");
   const data = await response.json();
-  const char_base_hp = UserData.data.avatarInfoList[SelectId].fightPropMap["1"].val;
-  let char_base_attck = UserData.data.avatarInfoList[SelectId].fightPropMap["4"].val;
-  const char_base_deff = UserData.data.avatarInfoList[SelectId].fightPropMap["7"].val;
-  const char_base_elm = data.ステータス.基礎元素熟知[toString((CharAdvanceRank + 2) * 10) + "+"];
-  const char_base_elm_charge = 1 + data.ステータス.基礎元素チャージ効率[toString((CharAdvanceRank + 2) * 10) + "+"]
-  const char_base_cr = data.ステータス.基礎会心率[toString((CharAdvanceRank + 2) * 10) + "+"];
-  const char_base_cd = data.ステータス.基礎会心ダメージ[toString((CharAdvanceRank + 2) * 10) + "+"];
+  const char_base_hp = UserData.data.avatarInfoList[toString(SelectId)].fightPropMap["1"].val;
+  let char_base_attck = UserData.data.avatarInfoList[toString(SelectId)].fightPropMap["4"].val;
+  const char_base_deff = UserData.data.avatarInfoList[toString(SelectId)].fightPropMap["7"].val;
+  const char_base_elm = data.ステータス.基礎元素熟知[toString((parseInt(CharAdvanceRank) + 2) * 10) + "+"];
+  const char_base_elm_charge = 1 + data.ステータス.基礎元素チャージ効率[toString((parseInt(CharAdvanceRank) + 2) * 10) + "+"]
+  const char_base_cr = data.ステータス.基礎会心率[toString((parseInt(CharAdvanceRank) + 2) * 10) + "+"];
+  const char_base_cd = data.ステータス.基礎会心ダメージ[toString((parseInt(CharAdvanceRank) + 2) * 10) + "+"];
   const dmg_buff_type = parseInt(data.ステータス.基礎ダメージバフ.元素);
   let char_base_dmg_buff = 0;
   if (dmg_buff_type == char_propaty[0])
   {
-    char_base_dmg_buff = parseFloat(data.ステータス.基礎ダメージバフ.数値[toString((CharAdvanceRank + 2) * 10) + "+"]);
+    char_base_dmg_buff = parseFloat(data.ステータス.基礎ダメージバフ.数値[toString((parseInt(CharAdvanceRank) + 2) * 10) + "+"]);
   }
 
   if (selectedCharId == 62 || selectedCharId == 70)

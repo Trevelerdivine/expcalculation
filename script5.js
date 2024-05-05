4d9676ec0f85d4b5b0c85553fd3e3fd2e63422ae
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-05-05 14:54:42 +0900

    test

--- script5.js
+++ script5.js
@@ -1514,6 +1514,11 @@ async function calculateEnemyProps(charDebuff, weaponDebuff) {
 
   // 防御補正計算
   const deffCorrection = (CharLevel + 100) / ((1 - charDebuff[2]) * (1 - charDebuff[1] - weaponDebuff[1] - enemyDeffDebuff) * (enemyLevel + 100) + CharLevel + 100);
+
+  console.log((CharLevel + 100));
+  console.log((1 - charDebuff[2]));
+  console.log((1 - charDebuff[1] - weaponDebuff[1] - enemyDeffDebuff));
+  console.log((enemyLevel + 100));
   console.log(deffCorrection);
 
   // 補正係数の計算

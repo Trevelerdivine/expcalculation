77b7fbd2a150c2edc3a2b4b5357e821a158b2f9f
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-05-22 05:49:08 +0900

    test

--- script7.js
+++ script7.js
@@ -1895,6 +1895,7 @@ async function SetMyAfStatus(){
     let AfSutatusList = [];
     let MainPropList = [];
     let SubPropList = [];
+    let MainStatus;
     const AfArray = [4,2,5,1,3]
     const subStatusBaseIndex = [0.583, 0.729, 2.331, 0.648, 0.583, 0.389, 0.777, 29.875, 1.945, 2.315, 0.583, 0.583, 0.583, 0.583, 0.583, 0.583, 0.583, 0.583, 0];
     // JSON配列の各要素を走査して、"itemId" の要素を取り出す
@@ -1907,7 +1908,8 @@ async function SetMyAfStatus(){
             status.flat.reliquarySubstats.map(stat => {
                 SubPropList.push([SubstatusData[stat.appendPropId]["id"], Math.round(stat.statValue / SubstatusData[stat.appendPropId]["基礎数値"]) * subStatusBaseIndex[SubstatusData[stat.appendPropId]["id"]]]);
             });
-            MainPropList.push([SubstatusData[status.flat.reliquaryMainstat.mainPropId]["id"], Math.round(status.flat.reliquaryMainstat.statValue / SubstatusData[status.flat.reliquaryMainstat.mainPropId]["基礎数値"]) * SubstatusData[status.flat.reliquaryMainstat.mainPropId]["基礎数値"]]);
+            MainStatus = [SubstatusData[status.flat.reliquaryMainstat.mainPropId]["id"], Math.round(status.flat.reliquaryMainstat.statValue / SubstatusData[status.flat.reliquaryMainstat.mainPropId]["基礎数値"]) * SubstatusData[status.flat.reliquaryMainstat.mainPropId]["基礎数値"]]
+            MainPropList.push(MainStatus);
             tempAfStatusList.push([MainPropList, SubPropList]);
         }
     });

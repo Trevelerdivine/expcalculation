f5af0bb35498f27cb319fa6d493b88e8da175348
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-06-13 02:00:22 +0900

    fix

--- data/character/char_class/char_class5.js
+++ data/character/char_class/char_class5.js
@@ -8560,7 +8560,7 @@ class Lyney {
       }
   
       // JSON データを取得
-      const response = await fetch("./data/character/char_data/chiori.json");
+      const response = await fetch("../data/character/char_data/chiori.json");
       const data = await response.json();
     
       // 攻撃方法に応じてダメージ率を計算

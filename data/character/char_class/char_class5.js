89ba4870998343f378bb916c1641b0404ca04625
TravelerDivine <132146191+Trevelerdivine@users.noreply.github.com>
2024-06-23 23:38:45 +0900

    test

--- data/character/char_class/char_class5.js
+++ data/character/char_class/char_class5.js
@@ -5526,7 +5526,7 @@ class Lyney {
       }
     
       // JSON データを取得
-      const response = await fetch("./data/character/char_data/Clorinde.json");
+      const response = await fetch("../data/character/char_data/Clorinde.json");
       const data = await response.json();
     
       // 攻撃方法に応じてダメージ率を計算
